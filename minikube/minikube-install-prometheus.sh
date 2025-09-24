dad2e89eb535764b4852af08213d5a88ebfe9be7
labcabrera <lab.cabrera@gmail.com>
2025-09-24 18:28:06 +0200

    Initial structure.

---
+++ minikube/minikube-install-prometheus.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+NAMESPACE_MONITORING="platform"
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+echo "----------------------------------------------------------------------------"
+echo "Creating namespace..."
+echo "----------------------------------------------------------------------------"
+
+kubectl create namespace "$NAMESPACE_MONITORING" --dry-run=client -o yaml | kubectl apply -f -
+
+echo "----------------------------------------------------------------------------"
+echo "Installing Prometheus..."
+echo "----------------------------------------------------------------------------"
+
+helm install prometheus prometheus-community/kube-prometheus-stack \
+  --namespace "$NAMESPACE_MONITORING" \
+  --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false \
+  --set prometheus.prometheusSpec.podMonitorSelectorNilUsesHelmValues=false \
+  --set prometheus.prometheusSpec.ruleSelectorNilUsesHelmValues=false \
+  --set prometheus.prometheusSpec.retention=30d \
+  --set prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.resources.requests.storage=10Gi \
+  --set prometheus.prometheusSpec.serviceMonitorNamespaceSelector.any=true \
+  --set prometheus.prometheusSpec.serviceMonitorSelector.matchLabels.app=mcm-demo \
+  --set alertmanager.enabled=true \
+  --set grafana.enabled=true \
+  --set grafana.adminPassword=changeit \
+  --set grafana.persistence.enabled=true \
+  --set grafana.persistence.size=5Gi \
+  --set grafana.service.type=NodePort \
+  --set prometheus.service.type=NodePort \
+  --timeout=600s
+
+echo "----------------------------------------------------------------------------"
+echo "Configuring Ingress..."
+echo "----------------------------------------------------------------------------"
+
+kubectl apply -f ingress/grafana-ingress.yaml -n "$NAMESPACE_MONITORING"
+kubectl apply -f ingress/prometheus-ingress.yaml -n "$NAMESPACE_MONITORING"
