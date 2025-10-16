27edf9310d6bc428b6e4801a1af5e9b0fca2d32e
labcabrera <lab.cabrera@gmail.com>
2025-10-16 08:10:35 +0200

    Initial kustomization

--- minikube/minikube-install.sh
+++ minikube/minikube-install.sh
@@ -51,8 +51,6 @@ helm install argocd argo/argo-cd \
   --set server.service.type=NodePort \
   --set configs.secret.argocdServerAdminPassword='$2y$10$vAKJfNlItZH/h500v0DObOd5IFBAUJifgSLTiVpKzqJ2AKGhqozVy'
 
-# Create ArgoCD applications
-
 find ../argocd -name "*.yaml" -o -name "*.yml" | while read -r file; do
   kubectl apply -f "$file" -n "$NAMESPACE_TOOLS"
 done
