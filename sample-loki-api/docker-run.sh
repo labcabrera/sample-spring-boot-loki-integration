1536f45503ae6fceb4ff77cec7ab778184282cde
labcabrera <lab.cabrera@gmail.com>
2025-10-16 11:01:22 +0200

    Updated logging config.

--- sample-loki-api/docker-run.sh
+++ sample-loki-api/docker-run.sh
@@ -11,6 +11,8 @@ docker rmi labcabrera/sample-loki-api:latest
 docker build -t labcabrera/sample-loki-api:latest .
 
 docker run -d --name sample-loki-api -p 8081:8081 \
+  -e JAVA_OPTS="-Xms512m -Xmx1024m" \
+  -e LOG_LEVEL="DEBUG" \
   labcabrera/sample-loki-api:latest
 
 docker logs -f sample-loki-api
