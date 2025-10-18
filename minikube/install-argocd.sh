8f65badeec180707ad3f8d1f6bdcc590acd280b4
labcabrera <lab.cabrera@gmail.com>
2025-10-18 11:19:00 +0200

    Updated scripts.

--- minikube/install-argocd.sh
+++ minikube/install-argocd.sh
@@ -4,16 +4,11 @@ set -e
 
 NAMESPACE="sample-cicd"
 
-echo "----------------------------------------------------------------------------"
-echo " Installing ArgoCD and configuring applications..."
-echo "----------------------------------------------------------------------------"
-
 kubectl create namespace "$NAMESPACE" --dry-run=client -o yaml | kubectl apply -f -
 
 helm install argocd argo/argo-cd \
-  --namespace "sample-cicd" \
+  --namespace "$NAMESPACE" \
   -f config/argocd-values.yaml
-  
 
 find ../argocd -name "*.yaml" -o -name "*.yml" | while read -r file; do
   kubectl apply -f "$file" -n "$NAMESPACE"
