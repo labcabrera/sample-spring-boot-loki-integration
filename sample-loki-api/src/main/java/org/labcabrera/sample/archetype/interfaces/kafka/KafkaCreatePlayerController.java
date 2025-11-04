72d7f820777067467d764de64181a38776c622a9
labcabrera <lab.cabrera@gmail.com>
2025-11-04 10:54:39 +0100

    Fix dependencies.

--- sample-loki-api/src/main/java/org/labcabrera/sample/archetype/interfaces/kafka/KafkaCreatePlayerController.java
+++ sample-loki-api/src/main/java/org/labcabrera/sample/archetype/interfaces/kafka/KafkaCreatePlayerController.java
@@ -2,7 +2,10 @@ package org.labcabrera.sample.archetype.interfaces.kafka;
 
 import java.util.function.Consumer;
 
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.labcabrera.sample.archetype.domain.player.command.CreatePlayerCommand;
 import org.labcabrera.sample.archetype.domain.player.event.PlayerCreatedEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +15,19 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class KafkaCreatePlayerController {
 
+    @Autowired
+    private CommandGateway commandGateway;
+
     @Bean
     public Consumer<PlayerCreatedEvent> createPlayer() {
         return event -> {
-            log.info("Player created event published: {}", event);
-            // Stream will publish the event to player-created
+            log.info("Player created << {}", event);
+            var command = new CreatePlayerCommand(
+                event.getPlayerId(),
+                event.getName(),
+                event.getEmail(),
+                event.getElo());
+            commandGateway.send(command);
         };
     }
 }
