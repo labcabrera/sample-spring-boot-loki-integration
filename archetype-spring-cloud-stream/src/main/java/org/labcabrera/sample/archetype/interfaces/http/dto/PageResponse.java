a611e9f1196708184075ea0145972d5569a76a6d
labcabrera <lab.cabrera@gmail.com>
2025-11-12 23:25:19 +0000

    Initial API.

--- archetype-spring-cloud-stream/src/main/java/org/labcabrera/sample/archetype/interfaces/http/dto/PageResponse.java
+++ archetype-spring-cloud-stream/src/main/java/org/labcabrera/sample/archetype/interfaces/http/dto/PageResponse.java
@@ -7,8 +7,12 @@ import org.springframework.data.domain.Page;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @Schema(description = "Paginated response")
 public class PageResponse<T> {
--- archetype-spring-cloud-stream/src/main/java/org/labcabrera/sample/archetype/interfaces/http/dto/Pagination.java
+++ archetype-spring-cloud-stream/src/main/java/org/labcabrera/sample/archetype/interfaces/http/dto/Pagination.java
@@ -1,16 +1,27 @@
 package org.labcabrera.sample.archetype.interfaces.http.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Value
-@AllArgsConstructor
+@Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Pagination information")
 public class Pagination {
 
+    @Schema(description = "Current page number (0-based)", example = "0")
     private int page;
+
+    @Schema(description = "Number of items per page", example = "20")
     private int size;
+
+    @Schema(description = "Total number of elements", example = "100")
     private long totalElements;
+
+    @Schema(description = "Total number of pages", example = "5")
     private int totalPages;
 }
